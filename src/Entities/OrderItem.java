4a7d8b014eba94daa38f90e05c6b0ce3b9c8477c
Jales Lana Attie de Castro <jales.la.na2.0@gmail.com>
2019-07-17 21:53:51 -0300

    Conclusão classe OrderItem

--- src/Entities/OrderItem.java
+++ src/Entities/OrderItem.java
@@ -1,26 +1,23 @@
 package Entities;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
-public class OrderItem {
-	private Integer quantity;
-	private Double price;
-	private List<Product> proList = new ArrayList<>();
+
+public class OrderItem{
 	
-
-	public OrderItem(Integer quantity, Double price) {
+	private	Integer quantity;
+	private Double price;
+	
+	public OrderItem (Integer quantity, Double price) {
 		this.quantity = quantity;
 		this.price = price;
 	}
 	
-	public OrderItem () {
+	public OrderItem() {
 		
 	}
 
 	public Integer getQuantity() {
-		return this.quantity;
+		return quantity;
 	}
 
 	public void setQuantity(Integer quantity) {
@@ -28,46 +25,16 @@ public class OrderItem {
 	}
 
 	public Double getPrice() {
-		return this.price;
+		return price;
 	}
 
 	public void setPrice(Double price) {
 		this.price = price;
 	}
 	
-	public List<Product> getProList() {
-		return this.proList;
+	
+	public Double subTotal() {
+		return getQuantity()*getPrice();
 	}
 	
-	public void addItem(Product item){
-	
-			this.proList.add(item);
-		
-		
-	}
-	
-	public void removeItem(Product item) {
-		int cont;
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("How many itens do you want to remove");
-		cont = scan.nextInt();
-		
-		for (int i = 0; i < cont; i++) {
-			
-			this.proList.remove(item);
-			
-		}
-		
-		
-	}
-	
-	
-	public Double subTotal(List list) {
-		
-		
-		return 2.0;
-	}
-	
-
 }
